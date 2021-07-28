ffa59cc992537826728ba61e10d99b4a6bacf00c
Your Name <you@example.com>
2021-07-27 23:40:10 -0400

    REevo: refine v1.02

--- gdvb/evolutionary/evo_bench.py
+++ gdvb/evolutionary/evo_bench.py
@@ -158,25 +158,30 @@ class EvoBench:
         # 2) prune overly easy and overly hard levels
         # 3) stop when necessary, go to analyze state
         if self.state == EvoState.Refine:
+
             if self.refine_iterations == 3:
                 next_ca_configs = self.first_refine(evo_step)
             else:
-                self.state = EvoState.Done
-                # next_ca_configs = self.refine(evo_step)
-            self.refine_iterations -= 1
+                #next_ca_configs = self.refine(evo_step)
+                next_ca_configs = self.first_refine(evo_step)
 
-            goto_next_flags = []
-            for p in self.evo_params:
-                this_range = [F(x) for x in ca_configs['parameters']['range'][p]]
-                next_range = [F(x) for x in next_ca_configs['parameters']['range'][p]]
-                this_level = ca_configs['parameters']['level'][p]
-                next_level = next_ca_configs['parameters']['level'][p]
-                goto_next_flags += [this_range == next_range and this_level == next_level]
-                print(this_level, next_level)
-                print(this_range, next_range)
-            if all(x for x in goto_next_flags):
+            self.refine_iterations -= 1
+            if self.refine_iterations == 0:
+                next_ca_configs = None
                 self.state = EvoState.Done
-            print(goto_next_flags)
+            else:
+                goto_next_flags = []
+                for p in self.evo_params:
+                    this_range = [F(x) for x in ca_configs['parameters']['range'][p]]
+                    next_range = [F(x) for x in next_ca_configs['parameters']['range'][p]]
+                    this_level = ca_configs['parameters']['level'][p]
+                    next_level = next_ca_configs['parameters']['level'][p]
+                    goto_next_flags += [this_range == next_range and this_level == next_level]
+                    print(this_level, next_level)
+                    print(this_range, next_range)
+                if all(x for x in goto_next_flags):
+                    self.state = EvoState.Done
+                print(goto_next_flags)
 
         if self.state == EvoState.Done:
             print('haha, Done, do some analyze now!')
@@ -248,77 +253,9 @@ class EvoBench:
 
         return ca_configs_new
 
-    def refine(self):
-        pass
-
-    def evolve2(self, evo_step):
-        evo_params = self.evo_configs['parameters']
-        assert len(evo_params) == 2
-        arity = self.evo_configs['arity']
-        inflation_rate = self.evo_configs['inflation_rate']
-        deflation_rate = self.evo_configs['deflation_rate']
-
+    def refine(self, evo_step):
         ca_configs = evo_step.benchmark.ca_configs
         ca_configs_new = copy.deepcopy(ca_configs)
-
-        for i, f in enumerate(evo_step.factors):
-            f = copy.deepcopy(f)
-            print(f'Working on factor: {f}')
-
-            axes = list(range(len(evo_params)))
-            axes.remove(i)
-            axes = [i] + axes
-            raw = list(evo_step.nb_solved.values())[0].transpose(axes)
-            print(raw)
-
-            # TODO ???
-            # cut levels that are too easy and too hard
-            nb_property = ca_configs['parameters']['level']['prop']
-            min_cut = [np.all(x == nb_property) for x in raw]
-            min_cut = min_cut.index(False) if False in min_cut else None
-            max_cut = [np.all(x == 0) for x in reversed(raw)]
-            max_cut = max_cut.index(False) if False in max_cut else None
-            print('min-max:', min_cut, max_cut)
-
-            # TODO ???
-            # set global_min and global_max
-            if min_cut and min_cut > 0:
-                self.global_min[f.type] = True
-            if max_cut and max_cut > 0:
-                self.global_max[f.type] = True
-
-            # find lower and upper boders
-            if not self.global_min[f.type] and not self.global_max[f.type]:
-                subdivision = False
-                f.set_start_end(f.start*deflation_rate, f.end*inflation_rate)
-            elif not self.global_min[f.type] and self.global_max[f.type]:
-                subdivision = False
-                f.set_start(f.start*deflation_rate)
-            elif self.global_min[f.type] and not self.global_max[f.type]:
-                subdivision = False
-                f.set_end(f.end*inflation_rate)
-            else:
-                subdivision = True
-
-            # cut easy or hard
-            new_start = f.start + min_cut * f.step if min_cut else None
-            new_end = f.end - max_cut * f.step if max_cut else None
-            if new_start and new_end:
-                f.set_start_end(new_start, new_end)
-            elif new_start and not new_end:
-                f.set_start(new_start)
-            elif not new_start and new_end:
-                f.set_end(new_end)
-            else:
-                pass
-
-            # subdivide the level plane
-            if subdivision:
-                f.subdivision(arity)
-
-            start, end, levels = f.get()
-            ca_configs_new['parameters']['level'][f.type] = levels
-            ca_configs_new['parameters']['range'][f.type] = [start, end]
 
         return ca_configs_new
 
