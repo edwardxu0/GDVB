813e7338c3547f8fb6bd0a67f0dd8946af095751
Edward <dxu@cse.unl.edu>
2021-08-21 17:27:57 -0400

    s2

--- scripts/scancel_JohHoldMaxReQueue.py
+++ scripts/scancel_JohHoldMaxReQueue.py
@@ -7,8 +7,8 @@ cmd = f'squeue -u dx3yy > {tmp_file}'
 os.system(cmd)
 jobs = [x for x in open(tmp_file,'r').readlines() if 'JobHoldMaxRequeue' in x]
 for j in jobs:
-    tks = j.split(' ')
-    idaa = int(tks[12])
+    tks = j.strip().split(' ')
+    idaa = int(tks[0])
     cmd = f'scancel {idaa}'
     print(cmd)
     os.system(cmd)
