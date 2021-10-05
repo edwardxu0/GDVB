ce09a225499aa18d37cf9362c887f0f0f954d95f
Edward <dxu@cse.unl.edu>
2021-10-05 11:39:37 -0400

    s3 evo update

---
+++ scripts_study/s3_explore.sh
@@ -0,0 +1,6 @@
+# python -m gdvb configs_s3/edz.toml evolutionary 10 2>./tmp/edz.err
+# python -m gdvb configs_s3/edp.toml evolutionary 10 2>./tmp/edp.err
+# python -m gdvb configs_s3/neurify.toml evolutionary 10 2>./tmp/neurify.err
+# python -m gdvb configs_s3/verinet.toml evolutionary 10 2>./tmp/verinet.err
+# python -m gdvb configs_s3/dnnf.toml evolutionary 10 2>./tmp/dnnf.err
+python -m gdvb configs_s3/nnenum.toml evolutionary 10 2>./tmp/nnenum.err
