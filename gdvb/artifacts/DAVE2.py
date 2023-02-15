01263c7ecd1aaaf53fa8ed4eb357a41deff27fad
Edward <edward@Thelio.local>
2023-02-15 11:11:03 -0500

    added tools to convert GDVB benchmark to VNNLib format

--- gdvb/artifacts/DAVE2.py
+++ gdvb/artifacts/DAVE2.py
@@ -14,24 +14,27 @@ class DAVE2(Artifact):
         super().__init__(dnn_configs)
         self.input_shape = (3, 100, 100)
 
-    def generate_property(self, data_config, prop_id, epsilon, skip_layers, output_dir, seed, gamma=15):
+    def generate_property(
+        self, data_config, prop_id, epsilon, skip_layers, output_dir, seed, gamma=15
+    ):
         torch.manual_seed(seed)
         config = DataConfiguration(data_config)
         config.config["_STAGE"] = "val_test"
         config.config["shuffle"] = True
         config.config["batchsize"] = 1
 
-        config.config['teacher'] = {}
-        config.config['teacher']['path'] = config.config['test']['teacher']['path']
-        config.config['student'] = {}
-        config.config['student']['path'] = config.config['test']['student']['path']
+        config.config["teacher"] = {}
+        config.config["teacher"]["path"] = config.config["test"]["teacher"]["path"]
+        config.config["student"] = {}
+        config.config["student"]["path"] = config.config["test"]["student"]["path"]
         data_loader = get_data_loader(config)
 
         for i, (idx, _, sx, target) in enumerate(data_loader):
             if i == prop_id:
                 input_img_path = data_loader.dataset.samples[0][idx][0]
                 new_img_path = os.path.join(
-                    output_dir, f"{idx.item()}{os.path.splitext(input_img_path)[-1]}")
+                    output_dir, f"{idx.item()}{os.path.splitext(input_img_path)[-1]}"
+                )
                 shutil.copyfile(input_img_path, new_img_path)
 
                 npy_img_path = os.path.join(output_dir, f"{idx.item()}.npy")
@@ -39,32 +42,36 @@ class DAVE2(Artifact):
                 img = sx[0].numpy()
                 np.save(npy_img_path, img)
 
-                property_lines = ["from dnnv.properties import *\n",
-                                  "import numpy as np\n\n",
-                                  'N = Network("N")\n',
-                                  f'x = Image("{npy_img_path}")\n']
+                property_lines = [
+                    "from dnnv.properties import *\n",
+                    "import numpy as np\n\n",
+                    'N = Network("N")\n',
+                    f'x = Image("{npy_img_path}")\n',
+                ]
 
                 if not skip_layers or skip_layers == 0:
                     property_lines += [f"input_layer = 0\n"]
                 else:
                     property_lines += [f"input_layer = {skip_layers}\n"]
 
-                property_lines += ["output_layer = -2\n\n",
-                                   f"epsilon = {epsilon}\n",
-                                   f"gamma = {gamma} * np.pi / 180\n",
-                                   "output = N[input_layer:](x)\n",
-                                   "gamma_lb = np.tan(max(-np.pi / 2, (output - gamma) / 2))\n",
-                                   "gamma_ub = np.tan(min(np.pi / 2, (output + gamma) / 2))\n",
-                                   "Forall(\n",
-                                   "    x_,\n",
-                                   "    Implies(\n",
-                                   "        ((x - epsilon) < x_ < (x + epsilon)),\n",
-                                   "        (gamma_lb < N[input_layer:output_layer](x_) < gamma_ub),\n",
-                                   "    ),\n",
-                                   ")\n"]
+                property_lines += [
+                    "output_layer = -2\n\n",
+                    f"epsilon = {epsilon}\n",
+                    f"gamma = {gamma} * np.pi / 180\n",
+                    "output = N[input_layer:](x)\n",
+                    "gamma_lb = np.tan(max(-np.pi / 2, (output - gamma) / 2))\n",
+                    "gamma_ub = np.tan(min(np.pi / 2, (output + gamma) / 2))\n",
+                    "Forall(\n",
+                    "    x_,\n",
+                    "    Implies(\n",
+                    "        ((x - epsilon) < x_ < (x + epsilon)),\n",
+                    "        (gamma_lb < N[input_layer:output_layer](x_) < gamma_ub),\n",
+                    "    ),\n",
+                    ")\n",
+                ]
 
-                property_path = os.path.join(
-                    output_dir, f"robustness_{i}_{epsilon}.py")
+                property_path = os.path.join(output_dir, f"robustness_{i}_{epsilon}.py")
                 with open(property_path, "w+") as property_file:
                     property_file.writelines(property_lines)
+                print(i, target, prop_id)
                 break
--- gdvb/misc/config.py
+++ gdvb/misc/config.py
@@ -5,19 +5,19 @@ import pathlib
 
 
 def configure(args):
-    config_file = open(args.configs, 'r').read()
+    config_file = open(args.configs, "r").read()
     configs = toml.loads(config_file)
 
-    configs['root'] = os.path.join(args.result_dir, f'{configs["name"]}.{args.seed}')
-    configs['seed'] = args.seed
-    configs['task'] = args.task
-    configs['override'] = args.override
+    configs["root"] = os.path.join(args.result_dir, f'{configs["name"]}.{args.seed}')
+    configs["seed"] = args.seed
+    configs["task"] = args.task
+    configs["override"] = args.override
     if args.debug:
-        configs['logging_level'] = logging.DEBUG
+        configs["logging_level"] = logging.DEBUG
     elif args.dumb:
-        configs['logging_level'] = logging.WARN
+        configs["logging_level"] = logging.WARN
     else:
-        configs['logging_level'] = logging.INFO
+        configs["logging_level"] = logging.INFO
 
     settings = Settings(configs)
 
@@ -27,40 +27,43 @@ def configure(args):
 class Settings:
     def __init__(self, configs):
         self.precision = 4
-        self.name = configs['name']
-        self.root = configs['root']
-        self.seed = configs['seed']
-        self.task = configs['task']
-        self.logging_level = configs['logging_level']
-        self.override = configs['override']
+        self.name = configs["name"]
+        self.root = configs["root"]
+        self.seed = configs["seed"]
+        self.task = configs["task"]
+        self.logging_level = configs["logging_level"]
+        self.override = configs["override"]
 
-        self.dnn_configs = configs['dnn']
-        self.ca_configs = configs['ca']
+        self.dnn_configs = configs["dnn"]
+        self.ca_configs = configs["ca"]
 
-        self.training_configs = configs['train']
-        self.verification_configs = configs['verify']
-        self.evolutionary = configs['evolutionary'] if 'evolutionary' in configs else None
+        self.training_configs = configs["train"]
+        self.verification_configs = configs["verify"]
+        self.evolutionary = (
+            configs["evolutionary"] if "evolutionary" in configs else None
+        )
 
-        self.sub_dirs = ['dis_config', 'dis_model', 'dis_log', 'props', 'veri_log']
-        
-        if configs['train']['dispatch']['platform'] == 'slurm':
-            self.sub_dirs += ['dis_slurm']
-        if configs['verify']['dispatch']['platform'] == 'slurm':
-            self.sub_dirs += ['veri_slurm']
+        self.sub_dirs = ["dis_config", "dis_model", "dis_log", "props", "veri_log"]
 
-        self.answer_code = {'unsat': 1,
-                            'sat': 2,
-                            'unknown': 3,
-                            'timeout': 4,
-                            'memout': 4,
-                            'error': 5}
+        if configs["train"]["dispatch"]["platform"] == "slurm":
+            self.sub_dirs += ["dis_slurm"]
+        if configs["verify"]["dispatch"]["platform"] == "slurm":
+            self.sub_dirs += ["veri_slurm"]
+
+        self.answer_code = {
+            "unsat": 1,
+            "sat": 2,
+            "unknown": 3,
+            "timeout": 4,
+            "memout": 4,
+            "error": 5,
+        }
         self._make_dirs()
 
     def _make_dirs(self):
         pathlib.Path(self.root).mkdir(parents=True, exist_ok=True)
         pathlib.Path().mkdir(parents=True, exist_ok=True)
         for sd in self.sub_dirs:
-            attr = sd + '_dir'
+            attr = sd + "_dir"
             self.__setattr__(attr, os.path.join(self.root, sd))
-            pathlib.Path(self.__getattribute__(attr)).mkdir(
-                parents=True, exist_ok=True)
+            pathlib.Path(self.__getattribute__(attr)).mkdir(parents=True, exist_ok=True)
